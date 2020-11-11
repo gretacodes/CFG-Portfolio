504f55df77d60c6e3766de8f4c52675ff0e1f38b
Greta <71640543+gretacodes@users.noreply.github.com>
2020-11-11 11:12:02 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Nothing to see here.
+ */
+
+let lastname = "matulionyte";
+console.log(lastname);
+
+const currentYear = 2020;
+console.log(currentYear);
+
+const age = 27;
+console.log(age);
+
+const city = "vilnius";
+console.log(city);
+
+const fruit= [“apples”,”oranges”,”pears”,”melons”];
+console.log (fruit [2]);
+
+
